dc81a3bbf9c67a88fde100dee6d0819339372ec9
zhu7478848 <zhu7478848@163.com>
2017-09-13 22:56:06 +0800

    11

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,11 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		HttpClientTest test = new HttpClientTest();
+		test.get();
 	}
 
+	
+	
+	
 }
